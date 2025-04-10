1034d818efb5951b631bf071814c3dc8d74a29de
Michele Otta <131482012+micheleotta@users.noreply.github.com>
2025-04-09 23:22:13 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "pucpr",
-    database: "expo_criativa5"
+    password: "",
+    database: ""
 });
